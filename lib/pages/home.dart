d9b5d7f809da06be4552a3cf91f16c79cae214de
kingahmedino <mohammedoluwasesan@gmail.com>
2024-10-13 16:01:42 +0100

    feat: Implement home ui

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,14 +24,150 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
-        child: SafeArea(
-          child: Column(
-            children: [
-              RestateTopBar(
-                location: 'Saint Petersburg',
-                avatarUrl: 'https://picsum.photos/200/300',
-              )
-            ],
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RestateTopBar(
+                  location: 'Saint Petersburg',
+                  avatarUrl: 'https://picsum.photos/200/300',
+                ),
+                RestateHeroText(name: 'Marina'),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(20.0),
+                          decoration: const BoxDecoration(
+                            color: RestateTheme.primaryColor,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Column(
+                            children: [
+                              Text('BUY',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodySmall
+                                      ?.copyWith(color: Colors.white)),
+                              const SizedBox(height: 36),
+                              Text('1 034',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .displayLarge
+                                      ?.copyWith(
+                                        color: Colors.white,
+                                        fontSize: 40,
+                                      )),
+                              const SizedBox(height: 4),
+                              Text('offers',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodySmall
+                                      ?.copyWith(color: Colors.white)),
+                              const SizedBox(height: 28),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                      Expanded(
+                        child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          decoration: BoxDecoration(
+                            color: Colors.white70,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Column(
+                            children: [
+                              Text('RENT',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodySmall
+                                      ?.copyWith(
+                                          color:
+                                              RestateTheme.secondaryTextColor)),
+                              const SizedBox(height: 36),
+                              Text('2 212',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .displayLarge
+                                      ?.copyWith(
+                                        color: RestateTheme.secondaryTextColor,
+                                        fontSize: 40,
+                                      )),
+                              const SizedBox(height: 4),
+                              Text('offers',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodySmall
+                                      ?.copyWith(
+                                          color:
+                                              RestateTheme.secondaryTextColor)),
+                              const SizedBox(height: 28),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(28.0),
+                  ),
+                  child: const Column(
+                    children: [
+                      RestateFullHomeCard(
+                        title: 'Gladkova St., 25',
+                        imageUrl:
+                            'https://www.smithandassociates.com/uploads/qwerqwe%20(1).14.jpeg',
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: RestateHalfHomeCard(
+                              title: 'Gubina St., 11',
+                              imageUrl:
+                                  'https://www.picturecorrect.com/wp-content/uploads/2014/02/real-estate-photography-2.jpg',
+                              height: 360,
+                            ),
+                          ),
+                          SizedBox(width: 8.0),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                RestateHalfHomeCard(
+                                  title: 'Trefoleva St., 43',
+                                  imageUrl:
+                                      'https://www.digitalphotomentor.com/photography/2018/09/real-estate-photography-two-wall-rule-example-5.jpg',
+                                  height: 180,
+                                ),
+                                SizedBox(height: 8.0),
+                                RestateHalfHomeCard(
+                                  title: 'Sedova St., 42',
+                                  imageUrl:
+                                      'https://www.shalinimisra.com/wp-content/uploads/2022/08/living-room-space-1024x745-1.jpeg',
+                                  height: 180,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -40,6 +176,175 @@ class HomePage extends StatelessWidget {
 
   Color _createLighterShade(Color color, double factor) {
     return Color.lerp(color, Colors.white, factor)!;
+  }
+}
+
+class RestateHalfHomeCard extends StatelessWidget {
+  const RestateHalfHomeCard({
+    super.key,
+    required this.title,
+    required this.imageUrl,
+    this.height = 180,
+  });
+  final String title;
+  final String imageUrl;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Image.network(
+            imageUrl,
+            height: height,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 12.0,
+          right: 12.0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xFFD6C5B6).withOpacity(0.92),
+              borderRadius: BorderRadius.circular(32),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 10,
+                  top: 0,
+                  bottom: 0,
+                  child: Center(
+                    child: Text(
+                      title,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium
+                          ?.copyWith(fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Spacer(),
+                    Container(
+                      width: 36,
+                      height: 36,
+                      margin: const EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.4),
+                            blurRadius: 20,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.keyboard_arrow_right_rounded,
+                        color: RestateTheme.secondaryTextColor,
+                        size: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class RestateFullHomeCard extends StatelessWidget {
+  const RestateFullHomeCard({
+    super.key,
+    required this.title,
+    required this.imageUrl,
+    this.height = 220,
+  });
+  final String title;
+  final String imageUrl;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(28.0),
+          child: Image.network(
+            imageUrl,
+            height: height,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 10.0,
+          left: 20.0,
+          right: 20.0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xFFD6C5B6).withOpacity(0.92),
+              borderRadius: BorderRadius.circular(32),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  top: 0,
+                  bottom: 0,
+                  child: Center(
+                    child: Text(
+                      title,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium
+                          ?.copyWith(fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Spacer(),
+                    Container(
+                      width: 42,
+                      height: 42,
+                      margin: const EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.4),
+                            blurRadius: 20,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.keyboard_arrow_right_rounded,
+                        color: RestateTheme.secondaryTextColor,
+                        size: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
 
@@ -63,31 +368,67 @@ class RestateTopBar extends StatelessWidget {
           Container(
             padding: const EdgeInsets.all(12.0),
             decoration: BoxDecoration(
-              color: Theme.of(context).scaffoldBackgroundColor,
+              color: Colors.white.withOpacity(0.8),
               borderRadius: BorderRadius.circular(12.0),
             ),
             child: Row(
               children: [
                 const Icon(
                   Icons.location_on,
-                  size: 18,
+                  size: 16,
                   color: RestateTheme.secondaryTextColor,
                 ),
                 const SizedBox(width: 4),
                 Text(
                   location,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium
-                      ?.copyWith(color: RestateTheme.secondaryTextColor),
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: RestateTheme.secondaryTextColor,
+                        fontWeight: FontWeight.w500,
+                      ),
                 ),
               ],
             ),
           ),
-          CircleAvatar(
-            radius: 18,
-            backgroundImage: NetworkImage(avatarUrl),
+          Container(
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage(avatarUrl),
+            ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class RestateHeroText extends StatelessWidget {
+  final String name;
+
+  const RestateHeroText({
+    super.key,
+    required this.name,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Hi, $name',
+              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                    fontSize: 20.0,
+                  )),
+          const SizedBox(height: 6),
+          Text("let's select your\nperfect place",
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge
+                  ?.copyWith(height: 1.1, fontSize: 36)),
         ],
       ),
     );
